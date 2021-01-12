1aeb7f5b848bc4b0f98025e3ba14bc515bc2ade0
“Liz <liz.mwesh02@gmail.com>
2021-01-11 21:17:55 -0500

    Refactored code, created login and register resolvers, validating user data and sending errors if any. Install bcryptjs and jsonwebtoken

--- index.js
+++ index.js
@@ -1,45 +1,20 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const Post = require('./models/Post');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
-const typeDefs = gql`
-  type Post {
-    id: ID! # exclamation mark to make it a required field
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = { // create a resolver for each query/mutation/subscription
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
-    }
-  }
-};
-
-const server = new ApolloServer({  // set up apollo server
+const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true }) // start mongodb then return promise to start server on port
+  .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });  // start server on port then return promise of console.log
+    return server.listen({ port: 5000 });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
